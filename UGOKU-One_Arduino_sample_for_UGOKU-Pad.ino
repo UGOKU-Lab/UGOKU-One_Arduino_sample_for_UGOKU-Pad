f97dc507a6067080c207530d1c319e486460bec3
Tomohiro Aoki <aoki.tomohiro.12@gmail.com>
2026-02-04 14:28:18 +0900

    接続中にモーターが暴走するバグを修正

--- UGOKU-One_Arduino_sample_for_UGOKU-Pad.ino
+++ UGOKU-One_Arduino_sample_for_UGOKU-Pad.ino
@@ -1,8 +1,9 @@
-#include "UGOKU-Pad_Controller.h"   
-#include "MotorDriver.h"
-#include "ESP32Servo.h"               
 #include "Arduino_BMI270_BMM150.h"
+#include "ESP32Servo.h"
+#include "MotorDriver.h"
+#include "UGOKU-Pad_Controller.h"
 #include <Wire.h>
+
 
 UGOKUPadController UGOKUPad;
 bool isConnected = false;
@@ -16,15 +17,15 @@ void setup() {
   UGOKUPad.setConnectionHandlers(onConnect, onDisconnect);
 
   // モーターの入力値を初期化
-  UGOKUPad.write(19, 127);
-  UGOKUPad.write(17, 127);
+  UGOKUPad.setDefaultValue(19, 127);
+  UGOKUPad.setDefaultValue(17, 127);
 
   MotorDriver_begin();
 
   pinMode(2, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(13, OUTPUT);
-  pinMode(23, OUTPUT);  
+  pinMode(23, OUTPUT);
 
   // LED をオフ
   digitalWrite(2, HIGH);
@@ -36,7 +37,8 @@ void setup() {
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
-    while (1);
+    while (1)
+      ;
   }
 }
 
@@ -56,42 +58,44 @@ void onDisconnect() {
 
 void loop() {
   // 接続確認
-  if (!isConnected) return;
+  if (!isConnected)
+    return;
 
   // UGOKU Pad のデータを更新
-  if (!UGOKUPad.update()) return;
+  if (!UGOKUPad.update())
+    return;
 
   // DIPスイッチ状態取得
   const bool invertServo = (digitalRead(34) == HIGH);
   const bool invertMotor = (digitalRead(35) == HIGH);
 
   // LED制御
-  digitalWrite(2,!UGOKUPad.read(2));
-  digitalWrite(4,!UGOKUPad.read(4));
-  digitalWrite(13,!UGOKUPad.read(13));
+  digitalWrite(2, !UGOKUPad.read(2));
+  digitalWrite(4, !UGOKUPad.read(4));
+  digitalWrite(13, !UGOKUPad.read(13));
 
   // FET制御
-  digitalWrite(23,UGOKUPad.read(23));
+  digitalWrite(23, UGOKUPad.read(23));
 
-  #if 0 // モーター独立駆動モード
+#if 0 // モーター独立駆動モード
     float md1 = (UGOKUPad.read(19) / 127.0f) - 1.0f;
     float md2 = (UGOKUPad.read(17) / 127.0f) - 1.0f;
     if (invertMotor) { md1 = -md1; md2 = -md2; }
     Motor(md1, md2);
-  #endif
+#endif
 
-  #if 1 // モーター対向2輪1ジョイスティックモード
-    float stick_x_duty = (float)UGOKUPad.read(19) / 127.0f - 1.0f;
-    float stick_y_duty = (float)UGOKUPad.read(17) / 127.0f - 1.0f;
+#if 1 // モーター対向2輪1ジョイスティックモード
+  float stick_x_duty = (float)UGOKUPad.read(19) / 127.0f - 1.0f;
+  float stick_y_duty = (float)UGOKUPad.read(17) / 127.0f - 1.0f;
 
-    float m1 = stick_x_duty + stick_y_duty;
-    float m2 = stick_y_duty - stick_x_duty;
+  float m1 = stick_x_duty + stick_y_duty;
+  float m2 = stick_y_duty - stick_x_duty;
 
-    m1 = constrain(m1, -1.0f, 1.0f);
-    m2 = constrain(m2, -1.0f, 1.0f);
+  m1 = constrain(m1, -1.0f, 1.0f);
+  m2 = constrain(m2, -1.0f, 1.0f);
 
-    Motor(m1, m2);
-  #endif
+  Motor(m1, m2);
+#endif
 
   // Servo（DIPで反転）
   uint8_t s2 = UGOKUPad.read(14);
@@ -103,7 +107,6 @@ void loop() {
   servo1.write(s2);
   servo2.write(s3);
 
-
   float x, y, z;
 
   if (IMU.accelerationAvailable()) {
@@ -113,7 +116,6 @@ void loop() {
     UGOKUPad.write(101, fabsf(y * 100));
     UGOKUPad.write(102, fabsf(z * 100));
   }
-  
+
   delay(50);
 }
-  
